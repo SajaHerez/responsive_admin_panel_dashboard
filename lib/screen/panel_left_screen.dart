e02c39fef2040fe3a4d2b9426ebbbe8de3f2b7e2
Saja HZ <herezsaja2020@gmail.com>
2023-06-23 05:08:23 +0300

    coding panel left screen

--- lib/screen/panel_left_screen.dart
+++ lib/screen/panel_left_screen.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_admin_panel_dashboard/resource/app_colors.dart';
+
+import '../resource/app_padding.dart';
+import '../widget/charts.dart';
+import '../widget/pie_chart.dart';
+import '../widget/responsive_layout.dart';
+
+class Todo {
+  String name;
+  bool enable;
+  Todo({this.enable = true, required this.name});
+}
 
 class PanelLeftScreen extends StatefulWidget {
   const PanelLeftScreen({super.key});
@@ -8,11 +20,101 @@ class PanelLeftScreen extends StatefulWidget {
 }
 
 class _PanelLeftScreenState extends State<PanelLeftScreen> {
+  final List<Todo> _todos = [
+    Todo(name: "Purchase Paper", enable: true),
+    Todo(name: "Refill the inventory of speakers", enable: true),
+    Todo(name: "Hire someone", enable: true),
+    Todo(name: "Maketing Strategy", enable: true),
+    Todo(name: "Selling furniture", enable: true),
+    Todo(name: "Finish the disclosure", enable: true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
+      body: Stack(
+        children: [
+          if (ResponsiveLayout.isComputer(context))
+            Container(
+              color: AppColors.purpleLight,
+              width: 50,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: AppColors.purpleDark,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.P10 / 2,
+                      top: AppPadding.P10 / 2,
+                      right: AppPadding.P10 / 2),
+                  child: Card(
+                    color: AppColors.purpleLight,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      child: const ListTile(
+                        title: Text(
+                          "Products Sold",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        subtitle: Text(
+                          "18% of Products Sold",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        trailing: Chip(
+                          label: Text(
+                            "4,500",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+               const LineChartSample2(),
+               const PieChartSample2(),
+                Padding(
+                    padding: const EdgeInsets.only(
+                        left: AppPadding.P10 / 2,
+                        top: AppPadding.P10 / 2,
+                        right: AppPadding.P10 / 2,
+                        bottom: AppPadding.P10),
+                    child: Card(
+                      color: AppColors.purpleLight,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                          children: List.generate(
+                              _todos.length,
+                              (index) => CheckboxListTile(
+                                  title: Text(
+                                    _todos[index].name,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  value: _todos[index].enable,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _todos[index].enable = value ?? true;
+                                    });
+                                  }))),
+                    ))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/widget/pie_chart.dart
@@ -0,0 +1,222 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_admin_panel_dashboard/resource/app_colors.dart';
+
+import '../resource/app_padding.dart';
+
+class PieChartSample2 extends StatefulWidget {
+  const PieChartSample2({super.key});
+
+  @override
+  State<StatefulWidget> createState() => PieChart2State();
+}
+
+class PieChart2State extends State {
+  int touchedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: const EdgeInsets.only(
+      //     left: AppPadding.P10 / 2,
+      //     right: AppPadding.P10 / 2,
+      //     top: AppPadding.P10,
+      //     bottom: AppPadding.P10),
+      child: Card(
+        color: AppColors.purpleLight,
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: AspectRatio(
+          aspectRatio: 1.3,
+          child: Row(
+            children: <Widget>[
+              const SizedBox(
+                height: 18,
+              ),
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: PieChart(
+                    PieChartData(
+                      pieTouchData: PieTouchData(
+                        touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                          setState(() {
+                            if (!event.isInterestedForInteractions ||
+                                pieTouchResponse == null ||
+                                pieTouchResponse.touchedSection == null) {
+                              touchedIndex = -1;
+                              return;
+                            }
+                            touchedIndex = pieTouchResponse
+                                .touchedSection!.touchedSectionIndex;
+                          });
+                        },
+                      ),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 40,
+                      sections: showingSections(),
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Indicator(
+                    color: Color(0xff0293ee),
+                    text: 'First',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Indicator(
+                    color: Color(0xFFFFC300),
+                    text: 'Second',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Indicator(
+                    color: Color(0xFF6E1BFF),
+                    text: 'Third',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Indicator(
+                    color: Color(0xff13d38e),
+                    text: 'Fourth',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 28,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(4, (i) {
+      final isTouched = i == touchedIndex;
+      final fontSize = isTouched ? 25.0 : 14.0;
+      final radius = isTouched ? 60.0 : 50.0;
+      const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: const Color(0xff0293ee),
+            value: 40,
+            title: '40%',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: shadows,
+            ),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: const Color(0xfff8b250),
+            value: 30,
+            title: '30%',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: shadows,
+            ),
+          );
+        case 2:
+          return PieChartSectionData(
+            color: const Color(0xFF6E1BFF),
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: shadows,
+            ),
+          );
+        case 3:
+          return PieChartSectionData(
+            color: const Color(0xff13d38e),
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: shadows,
+            ),
+          );
+        default:
+          throw Error();
+      }
+    });
+  }
+}
+
+class Indicator extends StatelessWidget {
+  const Indicator({
+    super.key,
+    required this.color,
+    required this.text,
+    required this.isSquare,
+    this.size = 16,
+    this.textColor = Colors.white70,
+  });
+  final Color color;
+  final String text;
+  final bool isSquare;
+  final double size;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: isSquare ? BoxShape.rectangle : BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 13,
+            // fontWeight: FontWeight.bold,
+            color: textColor,
+          ),
+        )
+      ],
+    );
+  }
+}
